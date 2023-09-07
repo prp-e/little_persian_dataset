c5de7a11255d9dc641ee502e7a2fb37d219a0d63
Muhammadreza Haghiri <haghiri75@gmail.com>
2023-09-07 17:07:21 +0330

    working on the raw dataset

--- data_manipulator.py
+++ data_manipulator.py
@@ -12,3 +12,11 @@ input_data_list = []
 
 for row in input_data.iterrows():
     input_data_list.append(row[1]['body'])
+
+resulting_text = '\n'.join(input_data_list)
+
+raw_file = open(raw_file_name, 'w', encoding='utf-8')
+raw_file.write(resulting_text)
+raw_file.close()
+
+print("Raw data has been written successfully.")
