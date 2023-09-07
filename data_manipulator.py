2070992ef01c23005b274cb1572ead502889364e
Muhammadreza Haghiri <haghiri75@gmail.com>
2023-09-07 17:06:10 +0330

    added respective file names

--- data_manipulator.py
+++ data_manipulator.py
@@ -1,6 +1,14 @@
 import os
 import pandas as pd
 
+raw_file_name = "raw_data.txt"
+train_file_name = "train.txt"
+val_file_name = "val.txt"
+vocab_file_name = "vocab.txt"
+
 input_data = pd.read_csv('tasnim.csv')
 
-print(input_data.head())
+input_data_list = []
+
+for row in input_data.iterrows():
+    input_data_list.append(row[1]['body'])
